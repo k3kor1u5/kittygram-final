3605ac837ca6c89ea54290b0da4ab63f47c7f882
Valentine L. <gen.metsu@yandex.ru>
2025-11-11 20:25:24 +0300

    Eighteenth

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,7 +1,8 @@
 import os
-from django.core.management.utils import get_random_secret_key
 from pathlib import Path
+
 import environ
+from django.core.management.utils import get_random_secret_key
 
 env = environ.Env(
     DEBUG=(bool, False)
@@ -11,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = get_random_secret_key()
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True' #True будет только если True в .env
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'  # True будет только если True в .env
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
